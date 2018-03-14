4dd597f33eccc297864c2878b9a086cdbffc1794
Steven Deutsch <stevensdeutsch@yahoo.com>
2018-03-14 00:42:53 -0500

    The main() function

---
+++ kilo.c
@@ -0,0 +1,13 @@
+//
+//  kilo.c
+//  Kilo
+//
+//  Created by Steven Deutsch on 3/14/18.
+//  Copyright © 2018 Steven Deutsch. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
